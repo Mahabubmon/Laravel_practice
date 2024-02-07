e29b5a02109ce0328c3efa94acf4d1648f3f5d83
Mahabubur Rahman <mahabubmon@gmail.com>
2024-02-07 17:51:06 +0600

    validation

--- Laravel_practice/app/Http/Controllers/Api/StudentController.php
+++ Laravel_practice/app/Http/Controllers/Api/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Student;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
@@ -23,6 +24,42 @@ class StudentController extends Controller
                 'message' => 'No Records Found'
             ], 404);
 
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|digits:10',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->message()
+            ], 422);
+        } else {
+            $student = Student::create([
+                'name' => $request->name,
+                'course' => $request->course,
+                'email' => $request->email,
+                'phone' => $request->phone,
+            ]);
+            if ($student) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Student Created Successfully"
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'message' => "Someting went wrong"
+                ]);
+
+            }
         }
     }
 }
