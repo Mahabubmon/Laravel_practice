3d529e4b8b4e8c6dcf87ad4af7480415c16db368
Mahabubur Rahman <mahabubmon@gmail.com>
2024-02-07 18:56:52 +0600

    api data fetch

--- Laravel_practice/app/Http/Controllers/Api/StudentController.php
+++ Laravel_practice/app/Http/Controllers/Api/StudentController.php
@@ -39,7 +39,7 @@ class StudentController extends Controller
         if ($validator->fails()) {
             return response()->json([
                 'status' => 422,
-                'errors' => $validator->message()
+                'errors' => $validator->messages()
             ], 422);
         } else {
             $student = Student::create([
@@ -52,14 +52,87 @@ class StudentController extends Controller
                 return response()->json([
                     'status' => 200,
                     'message' => "Student Created Successfully"
-                ]);
+                ], 200);
             } else {
                 return response()->json([
                     'status' => 500,
                     'message' => "Someting went wrong"
-                ]);
+                ], 500);
 
             }
         }
     }
+    public function show($id)
+    {
+        $student = Student::find($id);
+        if ($student) {
+            return response()->json([
+                'status' => 200,
+                'students' => $student
+            ], 200);
+
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => "No such Student found"
+            ], 404);
+
+        }
+    }
+    public function edit($id)
+    {
+        $student = Student::find($id);
+        if ($student) {
+            return response()->json([
+                'status' => 200,
+                'students' => $student
+            ], 200);
+
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => "No such Student found"
+            ], 404);
+
+        }
+
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|digits:10',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ], 422);
+        } else {
+            $student = Student::find($id);
+            $student = Student::update([
+                'name' => $request->name,
+                'course' => $request->course,
+                'email' => $request->email,
+                'phone' => $request->phone,
+            ]);
+            if ($student) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Student Created Successfully"
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'message' => "Someting went wrong"
+                ], 500);
+
+            }
+        }
+    }
+
 }
