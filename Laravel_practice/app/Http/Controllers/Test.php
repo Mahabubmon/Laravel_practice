265eeba12e583c0468abdfa2d4ed4264ea648b98
Mahabubur Rahman <mahabubmon@gmail.com>
2024-02-18 16:01:53 +0600

    adding route

--- Laravel_practice/app/Http/Controllers/Test.php
+++ Laravel_practice/app/Http/Controllers/Test.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class Test extends Controller
 {
-    //
+    public function index()
+    {
+        return view('welcome');
+    }
+    public function about()
+    {
+        return view('welcome');
+    }
+    public function product()
+    {
+        return view('product');
+    }
 }
