8a3f294439623fed5a22065f2e60fc6f4fc31396
Mahabubur Rahman <mahabubmon@gmail.com>
2024-02-26 17:59:31 +0600

    new page added

--- Laravel_practice/resources/views/backend/pages/category/add.blade.php
+++ Laravel_practice/resources/views/backend/pages/category/add.blade.php
@@ -2,7 +2,7 @@
 
 @section('main-content')
 <div class="container">
-    <button class="btn btn-primary">Add Category</button>
+    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#add">Add Category</button>
     <table id="example" class="table table-striped" style="width:100%">
         <thead>
             <tr>
@@ -31,27 +31,7 @@
                     <button data-bs-toggle="modal" data-bs-target="#delete{{$category->id}}" class="btn btn-danger">
                         Delete</button>
 
-                    <!-- Delete Modal -->
-                    <div class="modal fade" id="delete{{$category->id}}" tabindex="-1" role="dialog">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="deleteModalLabel{{$category->id}}">Confirm Deletion</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    Are you sure you want to delete this category?
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
-                                    <a href="{{route('deletecategory' , $category->id)}}" type="button"
-                                        class="btn btn-primary">Yes</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Delete Modal -->
+
 
                 </td>
             </tr>
@@ -59,4 +39,42 @@
         </tbody>
     </table>
 </div>
+
+
+<!-- For add category Modal -->
+<div class="modal fade" id="add" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addModal">Add New Category</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="name">Category Name</label>
+                    <input type="text" class="name form-control" placeholder="Name" id="name">
+                </div>
+                <div class="form-group">
+                    <label for="des">Category Description</label>
+                    <input type="text" class="des form-control" placeholder="Description" id="des">
+                </div>
+                <div class="mb-3">
+                    <label for="status" class="form-label">Status</label>
+                    <select name="status" id="status" class="form-control status">
+                        <option value="">-----Select Status-----</option>
+                        <option value="1">Active</option>
+                        <option value="2">Inactive</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <button class="add-category form-control">Add Category</button>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End category Modal -->
 @endsection
