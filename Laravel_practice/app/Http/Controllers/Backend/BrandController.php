548acd6d96a5753104615b9f1a04c23035dc2c8e
Mahabubur Rahman <mahabubmon@gmail.com>
2024-03-03 10:01:52 +0600

    try to updload img

--- Laravel_practice/app/Http/Controllers/Backend/BrandController.php
+++ Laravel_practice/app/Http/Controllers/Backend/BrandController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Image;
 
 class BrandController extends Controller
 {
@@ -23,7 +24,10 @@ class BrandController extends Controller
         ]);
 
         if ($request->image) {
-
+            $image = $request->file('brand_image');
+            $customName = rand() . '.' . $image->getClientOriginalExtension();
+            $location = public_path('backend/brand/' . $customName);
+            Image::make($image)->resize(300, 200)->save($location);
         } else {
 
         }
