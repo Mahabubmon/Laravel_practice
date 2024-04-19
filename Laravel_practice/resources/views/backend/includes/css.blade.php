0f5e8e7e477a9f416ba98fa260adf483b6c66832
Mahabubmon <mahabubmon@gmail.com>
2024-04-19 22:37:34 +0600

    cleaning for new

--- Laravel_practice/resources/views/backend/includes/css.blade.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="{{asset('backend')}}/style.css" />
-<!-- Boxicons CDN Link -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
-<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet" />
-<link href="https://cdn.datatables.net/2.0.0/css/dataTables.bootstrap5.css" rel="stylesheet" />
